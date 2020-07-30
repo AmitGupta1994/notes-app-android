845b9ff11bae7b69a01b8babf657aa7d6154eb56
Amit Gupta <iamitgupta1994@gmail.com>
2020-07-30 12:24:02 +0545

    [wip][dev]: working on MainActivity for showing the list and menu

--- app/src/main/java/com/amitgupta/notesapp/SplashActivity.kt
+++ app/src/main/java/com/amitgupta/notesapp/SplashActivity.kt
@@ -3,8 +3,16 @@ package com.amitgupta.notesapp
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.amitgupta.notesapp.adapter.NoteRecyclerViewAdapter
+import com.amitgupta.notesapp.room.DatabaseService
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var databaseService: DatabaseService
+
+    private lateinit var noteRecyclerViewAdapter: NoteRecyclerViewAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
