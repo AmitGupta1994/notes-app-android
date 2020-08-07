0e0ccad3fc029be13f6456edc729b20ff6590f30
Amit Gupta <iamitgupta1994@gmail.com>
2020-08-07 11:03:50 +0545

    [wip][dev]: adds onCreateOptionsMenu() to handle menu

--- app/src/main/java/NoteActivity.kt
+++ app/src/main/java/NoteActivity.kt
@@ -3,6 +3,7 @@ package com.amitgupta.notesapp
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.amitgupta.notesapp.room.DatabaseService
 
@@ -37,6 +38,26 @@ class NoteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add_note, menu)
+
+        val saveItem = menu?.findItem(R.id.action_save_note)
+        val updateItem = menu?.findItem(R.id.action_update_note)
+        val deleteItem = menu?.findItem(R.id.action_delete_note)
+
+        if (noteId == 0) {
+            saveItem!!.isVisible = true
+            updateItem!!.isVisible = false
+            deleteItem!!.isVisible = false
+        } else {
+            saveItem!!.isVisible = false
+            updateItem!!.isVisible = true
+            deleteItem!!.isVisible = true
+        }
+
+        return true
     }
 
 }
