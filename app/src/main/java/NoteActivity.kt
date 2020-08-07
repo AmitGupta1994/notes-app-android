674d27b14eab235fafbdda500b2e3c0bb969a02c
Amit Gupta <iamitgupta1994@gmail.com>
2020-08-07 11:05:10 +0545

    [wip][dev]: adds onOptionsItemSelected() to handle menu item click

--- app/src/main/java/NoteActivity.kt
+++ app/src/main/java/NoteActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.amitgupta.notesapp.room.DatabaseService
 
@@ -58,6 +59,25 @@ class NoteActivity : AppCompatActivity() {
         }
 
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            R.id.action_save_note -> {
+                true
+            }
+            R.id.action_update_note -> {
+                true
+            }
+            R.id.action_delete_note -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
